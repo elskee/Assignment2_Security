517820252c2d30779211be7f55f0952775eb154f
elskee <s3262707@vuw.leidenuniv.nl>
2025-11-17 15:40:29 +0100

    updated SQL code

--- dataclean.sql
+++ dataclean.sql
@@ -1,11 +1,13 @@
 
-SELECT cv.cve_id, f.filename, f.num_lines_added, f.num_lines_deleted, f.code_before, f.code_after, cc.cwe_id 
+select DISTINCT cve_id, hash, repo_url FROM(
+SELECT cv.cve_id, fx.hash, fx.repo_url, f.filename, f.num_lines_added, f.num_lines_deleted, f.code_before, f.code_after, f.diff_parsed, cc.cwe_id 
 FROM file_change f, commits c, fixes fx, cve cv, cwe_classification cc
 WHERE f.hash = c.hash 
 AND c.hash = fx.hash 
 AND fx.cve_id = cv.cve_id 
 AND cv.cve_id = cc.cve_id 
+AND fx.score >= 55
 AND f.programming_language = 'Python'
-AND cc.cwe_id = 'CWE-89'
+AND cc.cwe_id = 'CWE-89')
 
 
