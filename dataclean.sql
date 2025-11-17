e73aee65a1a6519c6520b2cbd993ec52103d23ed
elskee <s3262707@vuw.leidenuniv.nl>
2025-11-17 15:42:36 +0100

    Update SQL query to include commit URL in the selection

--- dataclean.sql
+++ dataclean.sql
@@ -1,6 +1,6 @@
 
-select DISTINCT cve_id, hash, repo_url FROM(
-SELECT cv.cve_id, fx.hash, fx.repo_url, f.filename, f.num_lines_added, f.num_lines_deleted, f.code_before, f.code_after, f.diff_parsed, cc.cwe_id 
+select DISTINCT commit_url, cve_id, hash, repo_url FROM(
+SELECT CONCAT(fx.repo_url, '/commit/', fx.hash) AS commit_url, cv.cve_id, fx.hash, fx.repo_url, f.filename, f.num_lines_added, f.num_lines_deleted, f.code_before, f.code_after, f.diff_parsed, cc.cwe_id 
 FROM file_change f, commits c, fixes fx, cve cv, cwe_classification cc
 WHERE f.hash = c.hash 
 AND c.hash = fx.hash 
